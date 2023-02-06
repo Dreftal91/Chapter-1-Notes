cd692c546f8c86b6180e591b5d3ab73c245d9dcc
ixmo <ixmo@fresnou.org>
2023-02-06 13:26:03 -0800

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+console.log(28);
+
+//This is a comment
+
+//Numbers
+//Integers
+console.log (-3);
+console.log (0);
+console.log(8);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-0.222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222225);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("Hello World");
+console.log('!@#$ is also a string');
+console.log(`backticks by the 1`);
+
+//Arithmatic Operators (Math)
+console.log(31+4);
+console.log(14-2);
+console.log(31/4); //shift-8 gives *
+console.log(31%4); //% is MOD
+                   //it means remainder after dividing
+
+//Concatenation means to combine strings
+console.log("My name"+" is Ms. Bolin");
+console.log('My'+' '+'age'+' '+'is '+14+'.');
+console.log("Your age is\n17");
+console.log("Your age is/n17");
+
+// Template literal – plug in some calculated value, then build the string on the fly
+//use backticks `` next to the 1
+
+console.log(`15 divided by 10 = ${200/8}`);
